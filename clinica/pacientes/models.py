0c10c1b2601c7f0769cf137a2e5bdb2e3b247728
Cyduarte <cayoduartevidal@gmail.com>
2024-08-30 23:52:06 -0300

    Fix

--- clinica/pacientes/models.py
+++ clinica/pacientes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Paciente(models.Model):
     nome = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11, unique=True)
+    cpf = models.CharField(max_length=12, unique=True)
     servico = models.CharField(max_length=100)
 
     def __str__(self):
